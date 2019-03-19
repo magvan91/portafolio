6284115447db9ed65e1aa89fc3fafa46d80c13f1
Marco García <mag.van.gam@gmail.com>
2019-03-19 17:46:09 -0600

    Add Component  Profile.vue

--- resources/assets/js/components/ProfileComponent.vue
+++ resources/assets/js/components/ProfileComponent.vue
@@ -4,14 +4,14 @@
     <div class="col-md-6">
         <div class="row">
           <div class="col-md-12 text-center">
-              <h1>{{name}} {{AppPaterno}} {{AppMaterno}} </h1>
+              <h1>{{Name}} {{AppPaterno}} {{AppMaterno}} </h1>
               <h2>Desarrollador Web</h2>
           </div>
           <div class="col-md-12">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pharetra lectus at fermentum pretium. Sed tincidunt lectus nunc, at accumsan lorem finibus non. Nam velit nisl, vulputate sed purus non, luctus ornare tortor. Morbi tincidunt pretium ex ut congue. Vivamus placerat libero non ante aliquet gravida. Morbi odio turpis, vestibulum non tellus non, lacinia vestibulum ipsum. Aliquam tincidunt gravida gravida. Integer vestibulum pulvinar dignissim. Aliquam fringilla nec velit a varius. Ut auctor volutpat erat eget dignissim. Praesent porta tortor nec odio aliquam, non rhoncus nunc sollicitudin. Cras auctor semper nulla, a condimentum nisl molestie ultricies.</div>
           <div class="col-md-12">
               <span>Edad:</span> {{Edad}}
-              <span>Carrera:</span> Ing. Tecnologías de la Información y Comunicación
+              <span>Carrera:</span> {{Carrera}}
               <span>Universidad:</span> {{Universidad}}
           </div>
         </div>
@@ -23,11 +23,12 @@
   export default {
     data(){
       return {
-        name: null,
+        Name: null,
         AppPaterno: null,
         AppMaterno: null,
         Universidad: null,
         Edad: null,
+        Carrera: null,
        }
     },
     mounted(){
@@ -39,6 +40,8 @@
           this.AppMaterno = response.data[0].AppMaterno;
           this.FechaNacimiento = response.data[0].fecha_nacimiento;
           this.Edad = this.birthday(response.data[0].fecha_nacimiento);
+          this.Universidad = response.data[0].universidad;
+          this.Carrera = response.data[0].carrera;
         }
       )
     },
